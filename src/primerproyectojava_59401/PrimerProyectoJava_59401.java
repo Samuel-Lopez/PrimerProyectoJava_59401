07c27aa03ba496ff5106e9e02d1b7bec28c43bf1
lopez <lopez@LAPTOP-7NRQOREU>
2021-03-03 13:18:21 -0600

    Primer proyecto en Java

---
+++ src/primerproyectojava_59401/PrimerProyectoJava_59401.java
@@ -0,0 +1,15 @@
+package primerproyectojava_59401;
+
+public class PrimerProyectoJava_59401 {
+
+    public static void main(String[] args) {
+        
+        int edad = 19;
+                
+        char letraFavorita = 'M';
+        
+        boolean Hombre = true;         
+        
+    }     
+    
+}
